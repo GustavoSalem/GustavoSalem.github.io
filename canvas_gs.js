25d6234852c24b8d03970861cb2759da242da073
Gustavo Silveira <72506847+GustavoSalem@users.noreply.github.com>
2025-11-29 22:20:32 -0300

    Remove menu item interactivity
    Removed interactivity code for menu items.

--- canvas_gs.js
+++ canvas_gs.js
@@ -64,10 +64,4 @@ const myChart = new Chart(ctx, {
     }
 });
 
-// Adicionar interatividade ao menu
-document.querySelectorAll('.menu-item').forEach(item => {
-    item.addEventListener('click', function () {
-        document.querySelectorAll('.menu-item').forEach(i => i.classList.remove('active'));
-        this.classList.add('active');
-    });
-}); 
+
